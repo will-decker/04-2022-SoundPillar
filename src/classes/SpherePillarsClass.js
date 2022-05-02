69100a4ec6aa300e4a19928d4c919591195863c2
willdecker <willdecker@gmail.com>
2022-05-02 18:12:21 -0400

    first commit

---
+++ src/classes/SpherePillarsClass.js
@@ -0,0 +1,16 @@
+class SpherePillarsClass {
+  constructor() {
+    this.bind();
+  }
+
+  init(scene) {
+    this.scene = scene;
+  }
+
+  update() {}
+
+  bind() {}
+}
+
+const _instance = new SpherePillarsClass();
+export default _instance;
