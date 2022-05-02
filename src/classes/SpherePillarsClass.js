f1a906e4650ffb661207a9ba4594ca5804194df3
willdecker <willdecker@gmail.com>
2022-05-02 18:52:42 -0400

    setup sphere pillar class and import model

--- src/classes/SpherePillarsClass.js
+++ src/classes/SpherePillarsClass.js
@@ -1,10 +1,23 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+
 class SpherePillarsClass {
   constructor() {
     this.bind();
+    this.modelLoader = new GLTFLoader();
   }
 
   init(scene) {
     this.scene = scene;
+    this.modelLoader.load('./assets/models/pillar.glb', (glb) => {
+      console.log(glb);
+      glb.scene.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          child.material = new THREE.MeshNormalMaterial();
+        }
+      });
+      this.scene.add(glb.scene);
+    });
   }
 
   update() {}
